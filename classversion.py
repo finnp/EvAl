fcd3a71ac14d972294eccf4d041f0371bbb3713a
Finn <derfinn@gmail.com>
2014-01-19 02:36:42 +0100

    debugging

--- classversion.py
+++ classversion.py
@@ -1,6 +1,6 @@
 # Lexi und Finn
 #
-# Evolutionärer Algorithmus zum Finden von Futter
+# Evolutionaerer Algorithmus zum Finden von Futter
 # mit Sensorik
 
 import random
@@ -25,7 +25,8 @@ class DNA_Sequence:
 
     def mutate(self):
         key = random.randrange(0, len(self.data))
-        self.data[key] = random.choice( self.data_value_set )
+        if(random.random() > 0.95):
+            self.data[key] = random.choice( self.data_value_set )
         return self
 
     def _generate_data(self):
@@ -57,7 +58,7 @@ class DNA:
         return self
 
 class Creature:
-    def __init__(self, dna, world, position = (0,0)):
+    def __init__(self, dna, world, position = (12,12)):
         self.dna = dna
         self.position = position
         self.food = 0
@@ -70,6 +71,18 @@ class Creature:
 
     def __str__(self):
         return "Food: {}, Vision: {}".format(self.food, self.food_sense_distance)
+
+    def print_world(self):
+        block_str = ""
+        for y in range(self.world.height):
+            for x in range(self.world.width):
+                if((x,y) == self.position):
+                    block_str += "@"
+                else:
+                    block_str += str(self.world.blocks[x][y])
+            block_str += "\n"
+        block_str += "\n"
+        return block_str
 
     # Breeds the child this and the other creature
     # Return new Creature
@@ -85,15 +98,19 @@ class Creature:
     def live(self, moves):
         for i in range(moves):
             self.act()
+        #print(self)
         return self.food
 
     # The creature acts upon the world
     def act(self):
+        #print(self.print_world())
+
         # If already at food, remove it
         if(self.world.is_food_at(self.position[0], self.position[1])):
             block = self.world.get_block(self.position[0], self.position[1])
             block.has_food = False
             self.food += 1
+            #print('> Collected Food!')
 
         direction = self._sense_food()
         if( direction ):
@@ -109,9 +126,9 @@ class Creature:
         
         directions = [(0,1),(1,0),(-1,0),(0,-1)]
         
-        for distance in range( self.food_sense_distance ):
+        for distance in range( self.food_sense_distance + 1 ):
             for x, y in directions:
-                if self.world.is_food_at(self.position[0] + x, self.position[1] + y):
+                if self.world.is_food_at(self.position[0] + distance * x, self.position[1] + distance * y):
                     return (x, y)
         return None
 
@@ -122,16 +139,24 @@ class Creature:
 
     def _move_food(self, direction):
         self._move(direction)
+        #print("> Move towards food")
         return self
 
     def _move_general(self):
         for step in self.move_general_path:
             self._move( step )
+        #print("> General move")
         return self
 
 class Block:
     def __init__(self, has_food = False):
         self.has_food = has_food
+
+    def __str__(self):
+        if self.has_food:
+            return "x"
+        else:
+            return "_"
 
 class World: 
     def __init__(self, width, height, blocks = None):
@@ -143,12 +168,21 @@ class World:
             self.blocks = []
             self.random_fill()
 
+    def __str__(self):
+        block_str = ""
+        for y in range(self.height):
+            for x in range(self.width):
+                block_str += str(self.blocks[x][y])
+            block_str += "\n"
+        block_str += "\n"
+        return block_str
 
     def random_fill(self):
+      self.blocks = []
       for x in range(self.width):
         self.blocks.append([])
         for y in range(self.height):
-            self.blocks[x].append( Block( random.random() > 0.5 ) )      
+            self.blocks[x].append( Block( random.random() > 0.99 ) )      
 
     def is_food_at(self, x, y):
         block = self.get_block(x, y)
@@ -171,9 +205,13 @@ class World:
                 new_blocks[x].append( self.blocks[x][y] )
         return World(self.width, self.height, new_blocks)
 
+vision_values = []
+
 class Generation:
     def __init__(self, size, creatures = None):
         self.size = size
+        self.average_food_value = -1
+        self.average_vision_value = -1
 
         if creatures:
             self.creatures = creatures
@@ -182,28 +220,50 @@ class Generation:
 
     def next_generation(self, world):
         for creature in self.creatures:
-            creature.live(15)
+            creature.live(40)
         # Sort creatures by fitness
-        self.creatures.sort(key = lambda c: c.food)
+        self.creatures.sort(key = lambda c: -c.food)
         # breed
         children = []
         for i in range(self.size):
             children.append( self.creatures[0].breed( self.creatures[1] ) )
 
         print(self.creatures[0])
+        self.average_food_value = self.average_food()
+        self.average_vision_value = self.average_food_sense_distance()
+
+        vision_values.append(self.average_vision_value)
+        
         return Generation(self.size, children)
+
+    def average_food(self):
+        food = 0
+        for creature in self.creatures:
+            food += creature.food
+        return food / len(self.creatures)
+
+    def average_food_sense_distance(self):
+        food_sense_distance = 0
+        for creature in self.creatures:
+            food_sense_distance += creature.food_sense_distance
+        return food_sense_distance / len(self.creatures)
+
 
     def _populate(self):
         self.creatures = []
         for i in range(self.size):
-            dna_sequence_sense_food =  DNA_Sequence(data_length = 1, data_value_set = [x for x in range(1,10)])
+            dna_sequence_sense_food =  DNA_Sequence(data_length = 1, data_value_set = [0, 10])
             dna_sequence_move_food = DNA_Sequence(data_length = 4, data_value_set = [(1,0),(0,1),(-1,0),(0,-1)])
             dna_sequence_move_general = DNA_Sequence(data_length = 4, data_value_set = [(1,0),(0,1),(-1,0),(0,-1)])
             dna = DNA(dna_sequences = [dna_sequence_sense_food, dna_sequence_move_food, dna_sequence_move_general])
             self.creatures.append( Creature(dna, world) )
 
+
+def average(l):
+    return sum(l) / float(len(l))
+
 if __name__ == "__main__":
-    world = World(100, 100)
+    world = World(25, 25)
     GENERATION_SIZE = 25
     GENERATION_NUMBER = 2500
 
@@ -212,5 +272,15 @@ if __name__ == "__main__":
 
     # Loop through generations
     for i in range(GENERATION_NUMBER):
+        #print( world )
         generation = generation.next_generation(world)
         world.random_fill()
+
+
+
+print("First vision average:" + str(average(vision_values[int(len(vision_values)/2):])))
+print("Second vision average:" + str(average(vision_values[:int(len(vision_values)/2)])))
+
+
+
+
